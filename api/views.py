71394aaa1074ff0f960e47e826f673dfc7149cbf
WebWorm <Ivan2002dav@gmail.com>
2018-04-01 01:28:46 +0300

    Added 'id'

--- api/views.py
+++ api/views.py
@@ -5,11 +5,11 @@ from .models import *
 def comments_list(request):
     result = []
     for each_comment in comment.objects.all():
-        result.append({ 'user': each_comment.user, 'text': each_comment.text })
+        result.append({'id': each_comment.pk, 'user': each_comment.user, 'text': each_comment.text })
     return JsonResponse(result, safe=False)
 
 def comment_id(request, id):
     result = []
     this_comment = comment.objects.get(pk= id)
-    result.append({ 'user': this_comment.user, 'text': this_comment.text })
+    result.append({'id': this_comment.pk, 'user': this_comment.user, 'text': this_comment.text })
     return JsonResponse(result, safe=False)
