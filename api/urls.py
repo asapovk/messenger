50a3d543d0a7ec17dbba40047f933bb4f944e552
WebWorm <Ivan2002dav@gmail.com>
2018-04-01 20:32:50 +0300

    Added 'comment_add function'

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comments/', views.comments_list),
-    path('comment/<id>/', views.comment_id)
+    path('comments/', views.comments_list, name='comments_list'),
+    path('comment/<id>/', views.comment_id, name='comment_id'),
+    path('comment/add/', views.comment_add, name='comment_add'),
 ]
